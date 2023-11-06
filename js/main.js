098c2de10371892838310649a0bb857961661ebb
Валерия <justlera007@gmail.com>
2023-11-06 14:09:20 +0300

    Отрисовка больших фото

--- js/main.js
+++ js/main.js
@@ -1,1 +1,3 @@
 import './thumbnails.js';
+
+
--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,4 +1,5 @@
 import { createPosts } from './data.js';
+import { renderBigPicture } from './big-picture.js';
 
 const picturesContainer = document.querySelector('.pictures');
 
@@ -6,20 +7,24 @@ const picturesTitle = picturesContainer.querySelector('.pictures__title');
 picturesTitle.classList.remove('visually-hidden');
 
 const pictureTemplate = document.querySelector('#picture')
-.content
-.querySelector('.picture');
+  .content
+  .querySelector('.picture');
 
 const picturesGallery = createPosts();
 
 const pictureGalleryFragment = document.createDocumentFragment();
 
 
-picturesGallery.forEach(({url, likes, comments}) => {
+picturesGallery.forEach(({ url, likes, comments }) => {
   const pictureElement = pictureTemplate.cloneNode(true);
 
   pictureElement.querySelector('.picture__img').src = url;
   pictureElement.querySelector('.picture__likes').textContent = likes;
   pictureElement.querySelector('.picture__comments').textContent = comments.length;
+
+  pictureElement.addEventListener('click', () => {
+    renderBigPicture({ url, likes, comments });
+  });
 
   pictureGalleryFragment.append(pictureElement);
 });
