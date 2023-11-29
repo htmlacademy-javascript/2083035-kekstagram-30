869fa02db6b83031a72ab6ff4d2484042c92d974
Валерия <justlera007@gmail.com>
2023-11-29 09:56:49 +0300

    Исправиа ошибку в user-message

--- js/form/user-message.js
+++ js/form/user-message.js
@@ -1,26 +1,49 @@
 import { isEscapeKey } from '../util/util.js';
 
-const userMessageError = document.querySelector('#error').content.querySelector('.error');
 const userMessageSuccess = document.querySelector('#success').content.querySelector('.success');
+const userMessageError = document.querySelector('#error').content.querySelector('.error');
 
-const closeErrorPopupMessage = () => {
-  document.removeEventListener('keydown', onDocumentKeydownError);
-  const errorPopup = document.querySelector('.error');
-  errorPopup.remove();
+const closeSuccessPopupMessage = () => {
+  const successPopup = document.querySelector('.success');
+  successPopup.remove();
+  document.removeEventListener('keydown', onDocumentKeydownSuccess);
+  document.removeEventListener('click', onOutSideClick);
 };
 
-const onOutSideErrorClick = (evt) => {
-  const errorPopupInner = document.querySelector('.error__inner');
-  if (evt.target !== errorPopupInner) {
-    closeErrorPopupMessage();
+const showSuccessSendDataMessage = () => {
+  const messageTemplate = userMessageSuccess.cloneNode(true);
+  messageTemplate.querySelector('.success__button').addEventListener('click', closeSuccessPopupMessage);
+  document.addEventListener('keydown', onDocumentKeydownSuccess);
+  document.addEventListener('click', onOutSideClick);
+  document.body.append(messageTemplate);
+};
+
+function onOutSideClick(evt) {
+  if (evt.target.closest('.success__inner') || evt.target.closest('.error__inner')) {
+    return;
   }
+  closeSuccessPopupMessage();
+}
+
+function onDocumentKeydownSuccess(evt) {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeSuccessPopupMessage();
+  }
+}
+
+const closeErrorPopupMessage = () => {
+  const errorPopup = document.querySelector('.error');
+  errorPopup.remove();
+  document.removeEventListener('keydown', onDocumentKeydownError);
+  document.removeEventListener('click', onOutSideClick);
 };
 
 const showErrorSendDataMessage = () => {
   const messageTemplate = userMessageError.cloneNode(true);
   messageTemplate.querySelector('.error__button').addEventListener('click', closeErrorPopupMessage);
   document.addEventListener('keydown', onDocumentKeydownError);
-  document.addEventListener('click', onOutSideErrorClick);
+  document.addEventListener('click', onOutSideClick);
   document.body.append(messageTemplate);
 };
 
@@ -28,34 +51,6 @@ function onDocumentKeydownError(evt) {
   if (isEscapeKey(evt)) {
     evt.preventDefault();
     closeErrorPopupMessage();
-  }
-}
-
-const closeSuccessPopupMessage = () => {
-  document.removeEventListener('keydown', onDocumentKeydownSuccess);
-  const successPopup = document.querySelector('.success');
-  successPopup.remove();
-};
-
-const onOutSideSuccessClick = (evt) => {
-  const successPopupInner = document.querySelector('.success__inner');
-  if (evt.target !== successPopupInner) {
-    closeSuccessPopupMessage();
-  }
-};
-
-const showSuccessSendDataMessage = () => {
-  const messageTemplate = userMessageSuccess.cloneNode(true);
-  messageTemplate.querySelector('.success__button').addEventListener('click', closeSuccessPopupMessage);
-  document.addEventListener('keydown', onDocumentKeydownSuccess);
-  document.addEventListener('click', onOutSideSuccessClick);
-  document.body.append(messageTemplate);
-};
-
-function onDocumentKeydownSuccess(evt) {
-  if (isEscapeKey(evt)) {
-    evt.preventDefault();
-    closeSuccessPopupMessage();
   }
 }
 
